dd3945f9e88dd62424e93294c24f21c7cde06847
zhangh <779389586@qq.com>
2023-05-04 17:47:48 +0800

    xiu

--- me/demo.py
+++ me/demo.py
@@ -1,5 +1,9 @@
-for i in range(1,10):
-    if i >= 9:
+for i in range(1,100):
+    if 20 > i >= 10:
         print(i)
-    if i <= 8:
-        print(i+100)
+    if i < 10:
+        print(i+10)
+    if 30 > i >= 20:
+        print(i*i)
+    if 40 > i >= 30:
+        print(i/2)
