d2056f5db90dd2e3ab0bfc6a7e29550a74598834
zhangsan0823 <109347751+zhangsan0823@users.noreply.github.com>
2023-05-04 17:55:08 +0800

    Delete demo.py

--- me/demo.py
+++
@@ -1,9 +0,0 @@
-for i in range(1,100):
-    if 20 > i >= 10:
-        print(i)
-    if i < 10:
-        print(i+10)
-    if 30 > i >= 20:
-        print(i*i)
-    if 40 > i >= 30:
-        print(i/2)
